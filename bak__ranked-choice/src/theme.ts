fd0b6681f24bb07bcc6d419d81185c1858b93046
Patrick Dukes <pdukes3@gmail.com>
2024-05-24 16:38:54 -0400

    Add MUI theme

--- bak__ranked-choice/src/theme.ts
+++ bak__ranked-choice/src/theme.ts
@@ -1,13 +1,52 @@
-export const theme = {
-  colors: {
-    primary: "#3da9fc",
-    alt: "#90b4ce",
-    secondary: "#094067",
-    tertiary: "#EF4565",
-    light: "#fffffe",
-    grey: "#5f6c7b",
+import { ThemeOptions } from "@mui/material/styles";
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#0d436b",
+      dark: "#094067",
+    },
+    secondary: {
+      main: "#aa1634",
+    },
+    background: {
+      default: "#f8f6f7",
+    },
+    divider: "#5F6C7B",
   },
-  fonts: {
-    main: "Roboto, sans-serif",
+  typography: {
+    fontFamily: '"Manrope", "Helvetica", "Arial", sans-serif',
+    h1: {
+      fontFamily: '"Lexend", "Helvetica", "Arial", sans-serif',
+    },
+    h2: {
+      fontFamily: '"Lexend", "Helvetica", "Arial", sans-serif',
+    },
+    h3: {
+      fontFamily: '"Lexend", "Helvetica", "Arial", sans-serif',
+    },
+    h4: {
+      fontFamily: '"Lexend", "Helvetica", "Arial", sans-serif',
+    },
+    h5: {
+      fontFamily: '"Lexend", "Helvetica", "Arial", sans-serif',
+    },
+    h6: {
+      fontFamily: '"Lexend", "Helvetica", "Arial", sans-serif',
+    },
+    subtitle1: {
+      fontFamily: '"Lexend", "Helvetica", "Arial", sans-serif',
+    },
+    caption: {
+      fontSize: "0.8rem",
+    },
+    button: {
+      fontFamily: '"Lexend", "Helvetica", "Arial", sans-serif',
+      fontWeight: 300,
+    },
+  },
+  shape: {
+    borderRadius: 8,
   },
 };

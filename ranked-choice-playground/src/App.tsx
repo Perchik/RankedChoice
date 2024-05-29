aae1e882ec66d285cf2f8dc84c55f055ab821fe7
Patrick Dukes <pdukes3@gmail.com>
2024-05-28 21:00:38 -0400

    Rename NameSpinner to WordSpinner, move to folder, and add test file

--- ranked-choice-playground/src/App.tsx
+++ ranked-choice-playground/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import NameSpinner from "./components/NameSpinner";
+import NameSpinner from "./components/WordSpinner/NameSpinner.component";
 
 const titles: string[] = [
   "Senator",

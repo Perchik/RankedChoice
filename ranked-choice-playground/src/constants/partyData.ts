d95ead826cb2a99383f9de2e1a8d3f6fd271bcf7
Patrick Dukes <pdukes3@gmail.com>
2024-06-05 11:37:37 -0400

    Update party preset image colors and styles. update the partydata color respectively.

--- ranked-choice-playground/src/constants/partyData.ts
+++ ranked-choice-playground/src/constants/partyData.ts
@@ -5,17 +5,17 @@ interface PartyData {
 }
 
 const parties: { [key: string]: PartyData } = {
-  red: { name: "Red Party", color: "#D00000", ordinal: 1 },
-  scarlet: { name: "Scarlet Party", color: "#E34234", ordinal: 3 },
-  orange: { name: "Orange Party", color: "#ff8800", ordinal: 2 },
-  amber: { name: "Amber Party", color: "#FFBF00", ordinal: 3 },
-  yellow: { name: "Yellow Party", color: "#ffff6b", ordinal: 1 },
+  red: { name: "Red Party", color: "#C31600", ordinal: 1 },
+  scarlet: { name: "Scarlet Party", color: "#FF2400", ordinal: 3 },
+  orange: { name: "Orange Party", color: "#FF7100", ordinal: 2 },
+  amber: { name: "Amber Party", color: "#FFae42", ordinal: 3 },
+  yellow: { name: "Yellow Party", color: "#FFDD55", ordinal: 1 },
   chartreuse: { name: "Chartreuse Party", color: "#9fff39", ordinal: 3 },
   green: { name: "Green Party", color: "#30a030", ordinal: 2 },
   teal: { name: "Teal Party", color: "#008080", ordinal: 3 },
-  blue: { name: "Blue Party", color: "#0000FF", ordinal: 1 },
+  blue: { name: "Blue Party", color: "#094067", ordinal: 1 },
   violet: { name: "Violet Party", color: "#862cff", ordinal: 3 },
-  purple: { name: "Purple Party", color: "#650065", ordinal: 2 },
+  purple: { name: "Purple Party", color: "#652265", ordinal: 2 },
   fuchsia: { name: "Fuchsia Party", color: "#d60a72", ordinal: 3 },
 };
 

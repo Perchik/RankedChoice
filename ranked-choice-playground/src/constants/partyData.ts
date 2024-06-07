d4d095b6581b47379f357045f914b8d783a9d9fa
Patrick Dukes <pdukes3@gmail.com>
2024-06-07 01:07:47 -0400

    add font color to partydata

--- ranked-choice-playground/src/constants/partyData.ts
+++ ranked-choice-playground/src/constants/partyData.ts
@@ -2,21 +2,81 @@ interface PartyData {
   name: string;
   color: string;
   ordinal: number;
+  fontColor: string;
 }
-
 const parties: { [key: string]: PartyData } = {
-  red: { name: "Red Party", color: "#C31600", ordinal: 1 },
-  scarlet: { name: "Scarlet Party", color: "#FF2400", ordinal: 3 },
-  orange: { name: "Orange Party", color: "#FF7100", ordinal: 2 },
-  amber: { name: "Amber Party", color: "#FFae42", ordinal: 3 },
-  yellow: { name: "Yellow Party", color: "#FFDD55", ordinal: 1 },
-  chartreuse: { name: "Chartreuse Party", color: "#9fff39", ordinal: 3 },
-  green: { name: "Green Party", color: "#30a030", ordinal: 2 },
-  teal: { name: "Teal Party", color: "#008080", ordinal: 3 },
-  blue: { name: "Blue Party", color: "#094067", ordinal: 1 },
-  violet: { name: "Violet Party", color: "#862cff", ordinal: 3 },
-  purple: { name: "Purple Party", color: "#652265", ordinal: 2 },
-  fuchsia: { name: "Fuchsia Party", color: "#d60a72", ordinal: 3 },
+  red: {
+    name: "Red Party",
+    color: "#C31600",
+    ordinal: 1,
+    fontColor: "#FFFFFF",
+  },
+  scarlet: {
+    name: "Scarlet Party",
+    color: "#FF2400",
+    ordinal: 3,
+    fontColor: "#FFFFFF",
+  },
+  orange: {
+    name: "Orange Party",
+    color: "#FF7100",
+    ordinal: 2,
+    fontColor: "#FFFFFF",
+  },
+  amber: {
+    name: "Amber Party",
+    color: "#FFae42",
+    ordinal: 3,
+    fontColor: "#000000",
+  },
+  yellow: {
+    name: "Yellow Party",
+    color: "#FFDD55",
+    ordinal: 1,
+    fontColor: "#000000",
+  },
+  chartreuse: {
+    name: "Chartreuse Party",
+    color: "#9fff39",
+    ordinal: 3,
+    fontColor: "#000000",
+  },
+  green: {
+    name: "Green Party",
+    color: "#30a030",
+    ordinal: 2,
+    fontColor: "#FFFFFF",
+  },
+  teal: {
+    name: "Teal Party",
+    color: "#008080",
+    ordinal: 3,
+    fontColor: "#FFFFFF",
+  },
+  blue: {
+    name: "Blue Party",
+    color: "#094067",
+    ordinal: 1,
+    fontColor: "#FFFFFF",
+  },
+  violet: {
+    name: "Violet Party",
+    color: "#862cff",
+    ordinal: 3,
+    fontColor: "#FFFFFF",
+  },
+  purple: {
+    name: "Purple Party",
+    color: "#652265",
+    ordinal: 2,
+    fontColor: "#FFFFFF",
+  },
+  fuchsia: {
+    name: "Fuchsia Party",
+    color: "#d60a72",
+    ordinal: 3,
+    fontColor: "#FFFFFF",
+  },
 };
 
 export const partyOrder: string[] = [

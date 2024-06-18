e84bdf571ab522170f19b0385f9d74df7df8fc74
Patrick Dukes <pdukes3@gmail.com>
2024-06-18 00:41:20 -0400

    Restructure the routing

--- ranked-choice-playground/src/App.tsx
+++ ranked-choice-playground/src/App.tsx
@@ -13,12 +13,13 @@ import MenuIcon from "@mui/icons-material/Menu";
 import "@fontsource/lexend";
 import "@fontsource/manrope";
 
-import Home from "./pages/Home";
+import ElectionSetupStepper from "./components/SetupWizard/SetupWizard";
 import EditBody from "./pages/EditBody";
 import Glossary from "./pages/Glossary";
 import logo from "./assets/logo.svg";
 
 import "./App.css";
+import HeadshotList from "./components/Headshot/HeadshotList";
 
 const App: React.FC = () => {
   return (
@@ -36,13 +37,15 @@ const App: React.FC = () => {
               style={{ height: "30px" }}
             />
           </Typography>
-          <Button color="inherit" component={Link} to="/">
-            Home
+          <Button color="inherit" component={Link} to="/setup">
+            Setup Election
           </Button>
           <Button color="inherit" component={Link} to="/edit-body">
             Edit Body SVG
           </Button>
-
+          <Button color="inherit" component={Link} to="/headshots">
+            View Headshots
+          </Button>
           <Button color="inherit" component={Link} to="/glossary">
             Glossary
           </Button>
@@ -50,9 +53,10 @@ const App: React.FC = () => {
       </AppBar>
       <Box>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/setup" element={<ElectionSetupStepper />} />
           <Route path="/edit-body" element={<EditBody />} />
           <Route path="/glossary" element={<Glossary />} />
+          <Route path="/headshots" element={<HeadshotList />} />
         </Routes>
       </Box>
     </Router>

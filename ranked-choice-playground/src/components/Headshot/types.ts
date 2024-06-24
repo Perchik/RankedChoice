8e335b0da042067db56e5f5bd60416efed536d3b
Patrick Dukes <pdukes3@gmail.com>
2024-06-24 10:50:25 -0400

    chore: Refactor Headshot component and update configuration types

--- ranked-choice-playground/src/components/Headshot/types.ts
+++ ranked-choice-playground/src/components/Headshot/types.ts
@@ -13,3 +13,21 @@ export interface ComplexionColor {
   hairColor: string;
   description: string; // Unused, but functions as a comment in the json file.
 }
+
+export interface HeadshotConfig {
+  skinColor: string;
+  hairColor: string;
+  hairId: string;
+  shirtColor: string;
+  suitColor: string;
+  lapelColor: string;
+  collarColor?: string;
+  tieType?: string;
+  pocketSquareType?: string;
+  outfitId?: string;
+  accessoryColor: string;
+}
+
+export interface SetHeadshotConfig {
+  (params: Partial<HeadshotConfig>): void;
+}
